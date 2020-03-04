c66e5ce33ec2354a34c334758d461083b971a218
Steven Ray Watson <swatson.emu@gmail.com>
2020-03-03 21:10:04 -0600

    A Github search application using vanilla javascript.

---
+++ app.js
@@ -0,0 +1,46 @@
+// Init Github
+const github = new Github();
+
+// Init UI
+const ui = new UI();
+
+async function getKeys() {
+  const response = await fetch("github.json")
+    .then(res => res.json())
+    .catch(err => console.log(err));
+
+  return response;
+}
+
+// Search input
+
+const searchUser = document.getElementById("search-user");
+
+// Add keyup listener
+
+searchUser.addEventListener("keyup", e => {
+  // Get input text
+
+  const userText = e.target.value;
+
+  if (userText !== "") {
+    // Make http call
+
+    github.getUser(userText).then(data => {
+      if (data.profile.message === "Not Found") {
+        // Show alert
+        ui.showAlert("User does not exist!", "alert alert-danger");
+        ui.clearProfile();
+      } else {
+        // Show profile
+        ui.clearAlert();
+        ui.showProfile(data.profile);
+        ui.showRepos(data.repos);
+      }
+    });
+  } else {
+    // Clear profile
+
+    ui.clearProfile();
+  }
+});
---
+++ github.js
@@ -0,0 +1,26 @@
+class Github {
+  constructor() {
+    this.client_id = "a011706b7e4326ca90c7";
+    this.client_secret = "2178764d9d06e322ac5ad73258c9a10d28a9d7a5";
+    this.repos_count = 5;
+    this.repos_sort = "created: asc";
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(
+      `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    };
+  }
+}
